23e5c3dae7250c3366aa35296a1ac6657a4ab855
cm2435 <cm2435@bath.ac.uk>
2023-12-12 08:15:40 +0000

    adding new experiment run artefacts

--- experimentation/launch_scripts/run_merge_adaptor.sh
+++ experimentation/launch_scripts/run_merge_adaptor.sh
@@ -1,1 +1,1 @@
-python ./adaptor_merge.py --base_model_name="mistralai/Mistral-7B-v0.1"  --adapter_model_name="./models/sft/final_checkpoint" --output_name="./models/zephyr_math_sft_merged" --hub_path="cm2435cm2435/zephyr_math_sft"
+python ./adaptor_merge.py --base_model_name="mistralai/Mistral-7B-v0.1"  --adapter_model_name="./models/dpo/final_checkpoint" --output_name="./models/zephyr_math_dpo_merged" --hub_path="cm2435cm2435/zephyr_math_dpo"
